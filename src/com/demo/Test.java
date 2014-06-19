0f47c07f57d483b9dbe606cc433cc178f36d9b31
onlysnail <Administrator@10.51.15.108>
2014-06-19 15:34:08 +0800

    11111111

--- src/com/demo/Test.java
+++ src/com/demo/Test.java
@@ -3,8 +3,9 @@ package com.demo;
 public class Test {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+        int a = 1;
+        int b = 1;
+        System.out.println(a+b);
     }
 
 }
